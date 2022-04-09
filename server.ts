6fbbdf3a45574209052785e49bb80ab770de2db4
Duarte-coder <gdbiel773@gmail.com>
2022-04-09 19:01:25 -0300

    att

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.use("/", express.static(resolve(__dirname, "./dist")));
 
-app.listen(process.env.PORT || 3000, (err) => {
+app.listen(process.env.PORT || 4000, (err) => {
   if (err) return console.log(err);
 
   console.log("Tudo nos conformes");
